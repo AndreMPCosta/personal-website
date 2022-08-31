f40786aa74f50288dd3dc6d0c2e0c1c04ab8b0e0
André Costa <andre.miguel.pcosta@gmail.com>
2022-08-31 19:25:19 +0100

    Projects skeleton

---
+++ src/content/projects.ts
@@ -0,0 +1,14 @@
+import { Project } from 'src/models/Project';
+
+const projects: Project[] = [
+  {
+    imageSrc: '',
+    title: 'Mangalibe',
+    subtitle: 'Acessories/Clothes Online Shop'
+  },
+  {
+    imageSrc: '',
+    title: 'Padel Checker',
+    subtitle: 'Tool For Padel Players'
+  }
+];
---
+++ src/models/Project.ts
@@ -0,0 +1,5 @@
+export interface Project {
+  imageSrc: string;
+  title: string;
+  subtitle: string;
+}
